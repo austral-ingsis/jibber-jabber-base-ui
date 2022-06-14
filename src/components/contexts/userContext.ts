52d6db2fb0caad07c309c3aeb31c6b10c598be27
flor-resoagli <florencia.resoagli@ing.austral.edu.ar>
2022-06-14 16:30:18 -0300

    parse token

--- src/components/contexts/userContext.ts
+++ src/components/contexts/userContext.ts
@@ -1,6 +1,21 @@
 import { User } from '../../data/users'
 import { createContext, useContext } from 'react'
+import keycloak from "../../Keycloak";
 
 export const UserContext = createContext<User>(undefined as unknown as User)
 
-export const useUserContext = () => useContext(UserContext)
+export const useUserContext = () => {
+
+    const kc = keycloak.tokenParsed
+
+    const user : User = {
+
+        id: kc?.sub ? kc.sub : '',
+        displayName: kc?.given_name + " " + kc?.family_name,
+        username: kc?.preferred_name
+
+    }
+
+    return user
+
+}

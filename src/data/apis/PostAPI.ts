864f38b9885edc20e4f6a1c5635a8990461db10f
flor-resoagli <florencia.resoagli@ing.austral.edu.ar>
2022-06-26 01:07:16 -0300

    Update PostAPI.ts

--- src/data/apis/PostAPI.ts
+++ src/data/apis/PostAPI.ts
@@ -1,4 +1,5 @@
 import {NewPost, Post, PostData} from "../posts";
+import keycloak from "../../Keycloak";
 
 const apiURL = "http://localhost:8083/posts"
 
@@ -6,12 +7,19 @@ class PostAPI implements PostData {
 
     answerPost(postId: string, answer: NewPost): Promise<Post> {
 
+        let token
+
+        if(keycloak.token) token = keycloak.token
+        else if(sessionStorage.getItem("token")) token = sessionStorage.getItem("token")
+
+
         return fetch(`${apiURL}/answerPost/${postId}`, {
             method: 'POST',
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Authorization' : 'Bearer ' + token
             },
             body: JSON.stringify(answer)
         }).then(r => (r.json()))
@@ -20,12 +28,19 @@ class PostAPI implements PostData {
 
     createPost(post: NewPost): Promise<Post> {
 
+        let token
+
+        if(keycloak.token) token = keycloak.token
+        else if(sessionStorage.getItem("token")) token = sessionStorage.getItem("token")
+
+
         return fetch(`${apiURL}/addPost`, {
             method: 'POST',
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Authorization' : 'Bearer ' + token
             },
             body: JSON.stringify(post)
         }).then(r => r.json())
@@ -36,12 +51,19 @@ class PostAPI implements PostData {
 
     getFeedPosts(): Promise<Post[]> {
 
+        let token
+
+        if(keycloak.token) token = keycloak.token
+        else if(sessionStorage.getItem("token")) token = sessionStorage.getItem("token")
+
+
         return fetch(`${apiURL}/getAllPosts/`, {
             method: 'GET',
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Authorization' : 'Bearer ' + token
             }
         }).then(res => {
 
@@ -55,12 +77,19 @@ class PostAPI implements PostData {
 
     getFullPostById(id: string): Promise<Post | undefined> {
 
+        let token
+
+        if(keycloak.token) token = keycloak.token
+        else if(sessionStorage.getItem("token")) token = sessionStorage.getItem("token")
+
+
         return fetch(`${apiURL}/getPost/${id}`, {
             method: 'GET',
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Authorization' : 'Bearer ' + token
             }
         }).then(r => r.json())
 
@@ -68,12 +97,19 @@ class PostAPI implements PostData {
 
     getPostsByUser(userId: string): Promise<Post[]> {
 
+        let token
+
+        if(keycloak.token) token = keycloak.token
+        else if(sessionStorage.getItem("token")) token = sessionStorage.getItem("token")
+
+
         return fetch(`${apiURL}/getUserPosts/${userId}`, {
             method: 'GET',
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Authorization' : 'Bearer ' + token
             }
         }).then(r => r.json())
             .then(data => {

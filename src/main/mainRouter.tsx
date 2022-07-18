24b6d07ab9c800bcb831c7ca16f77da88fbfd48f
flor-resoagli <florencia.resoagli@ing.austral.edu.ar>
2022-07-18 14:07:39 -0300

    fix :)

--- src/main/mainRouter.tsx
+++ src/main/mainRouter.tsx
@@ -13,10 +13,10 @@ export const MainRouter = () => {
   return (
     <Routes>
         {/*PUBLIC*/}
-        <Route path={'/'} element={<Login/>}/>
+        <Route path={'/'} element={<Home/>}/>
         {/*PRIVATE*/}
         {/*<Route path={'/but'} element={<button onClick={() => navigate('/home')}>To home</button>}/>*/}
-        <Route path="/home/*" element={ <Home/> }/>
+        {/*<Route path="/home/*" element={ <Home/> }/>*/}
         <Route path="/users/:userId" element={ <UserProfile/> }/>
         <Route path="/posts/:postId" element={ <PostPage/> }/>
     </Routes>

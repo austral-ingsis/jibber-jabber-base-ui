883cc257b390a8176b5bd7c70171584bb65f815a
flor-resoagli <florencia.resoagli@ing.austral.edu.ar>
2022-07-18 16:33:57 -0300

    fix :>

--- src/components/unauthenticated.tsx
+++ src/components/unauthenticated.tsx
@@ -1,14 +1,24 @@
 import React, {useEffect} from 'react'
 import keycloak from "../Keycloak";
+import {isNotUndefined} from "../utils/undefined";
 
 export const Unauthenticated = () => {
 
 
     useEffect(() => {
+        if(isNotUndefined(keycloak)){
             keycloak.login({
                 redirectUri:  'https://jbbrjbbr2202.store/',
             })
-        }, [])
+        }
+
+    }, [keycloak])
+
+    if(keycloak === undefined){
+        return(<div>Keycloak loading...</div>)
+    }
+
+
 
   return (
     <div>
